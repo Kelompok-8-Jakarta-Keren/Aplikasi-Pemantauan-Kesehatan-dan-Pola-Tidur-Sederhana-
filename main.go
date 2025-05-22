18a132f6f412b9b3f2cdb878706f8e0dc6aebfd4
chika-hellen <chikacalista628@gmail.com>
2025-05-22 10:03:51 +0700

    struktur data dan display menu

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+import (
+    "fmt"
+    "sort"
+    "strconv"
+    "time"
+)
+
+// Struktur data untuk riwayat tidur
+type SleepRecord struct {
+    Date       string    // Tanggal tidur
+    SleepTime  time.Time // Jam tidur
+    WakeTime   time.Time // Jam bangun
+    Duration   float64   // Durasi tidur (dalam jam)
+    Quality    string    // Kualitas tidur
+}
+// Fungsi untuk menampilkan menu utama
+func displayMenu() {
+    fmt.Println("=== APLIKASI PEMANTAUAN TIDUR ===")
+    fmt.Println("[1] Lihat Riwayat Tidur")
+    fmt.Println("[2] Tambahkan Riwayat Tidur Baru")
+    fmt.Println("[3] Edit Riwayat Tidur")
+    fmt.Println("[4] Hapus Riwayat Tidur")
+    fmt.Println("[5] Cari Riwayat Tidur Berdasarkan Tanggal")
+    fmt.Println("[6] Urutkan Riwayat Tidur Berdasarkan Durasi atau Tanggal")
+    fmt.Println("[7] Laporan Pola Tidur")
+    fmt.Println("[0] Keluar")
+    fmt.Print("Pilih menu> ")
+}
+
